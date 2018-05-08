3bd7e0342177e061f768a3fa888b354b709f5274
Catabriga <jhonatancatabriga@gmail.com>
2018-05-08 15:34:28 -0300

    Soluçao reduce

--- 4-reduce.test.js
+++ 4-reduce.test.js
@@ -3,11 +3,28 @@ const {testHelpers} = require('./lib')
 const patos = require('./patos')
 
 test('1 - Criar funçao que dados os patos, retorne o pato mais pesado. Usar reduce', t => {
-  const resultado =  0
-  t.is(resultado, 20)
+  
+  const maisPesado = (patos) => {
+    const maisPesadoReduce = (acc, pato) => {
+      if (pato.peso > acc.peso) {
+        return pato
+      }
+      return acc
+    }
+
+    return patos.reduce(maisPesadoReduce, {peso: 0})
+  }
+  const resultado =  maisPesado(patos)
+  t.is(resultado.peso, 20)
 })
 
 test('2 - Criar funçao que dados os patos, retorne o peso médio deles. Usar reduce', t => {
-  const resultado =  0
-  t.is(resultado, 12)
+  const media = (patos) => {
+    const total = patos.reduce((acc, pato) => {
+      return acc + pato.peso
+    }, 0)
+    return total / patos.length
+  }
+  const resultado =  media(patos)
+  t.is(Math.round(resultado), 12)
 })
